b498fa772eebd87344b81e72083bde15e32aff16
liu yi <yolaucn@gmail>
2021-09-25 18:34:28 +0800

    设置时间日期

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,8 +1,10 @@
 const Query=require('./query');
 const Mutation=require('./mutation');
+const {GraphQLDateTime}=require('graphql-iso-date');
 
 // 为模式字段提供解析函数
 module.exports={
     Query,
-    Mutation
+    Mutation,
+    DateTime:GraphQLDateTime
 };
--- src/schema.js
+++ src/schema.js
@@ -1,10 +1,14 @@
 const {gql}=require('apollo-server-express');
 // 使用GraphQL模式语言编制一个模式
 module.exports=gql`
+  scalar DateTime
+
   type Note{
     id:ID!
     content:String!
     author:String!
+    createdAt:DateTime!
+    updatedAt:DateTime!
   }
 
   type Query{
