d743cf65a99bfd04a2551a49a237c0f869f406aa
liu yi <yolaucn@gmail>
2021-09-25 18:18:32 +0800

    更新数据

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -5,6 +5,21 @@ module.exports = {
       author: 'liu yi'
     });
   },
+  updateNote: async (parent, { id, content }, { models }) => {
+    return await models.Note.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          content
+        }
+      },
+      {
+        new: true
+      }
+    );
+  },
   deleteNote: async (parent, { id }, { models }) => {
     try {
       await models.Note.findOneAndRemove({ _id: id });
