51ebaab29235292f22dc06a36d83020312dc0179
liu yi <yolaucn@gmail>
2021-09-25 20:47:59 +0800

    把用户信息添加到解析器上下文中

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express=require('express');
 const {ApolloServer}=require('apollo-server-express');
+const jwt=require('jsonwebtoken');
 
 // 导入本地模块
 const db=require('./db')
@@ -20,13 +21,32 @@ const app=express();
 //连接数据库
 db.connect(DB_HOST);
 
+const getUser=token=>{
+    if(token){
+        try{
+           // 返回通过令牌获取的用户信息
+           return jwt.verify(token,process.env.JWT_SECRET)
+        }catch(err){
+            // 如果令牌有问题 抛出错误
+            throw new Error('Session invalid');
+        }
+    }
+};
+
+
 
 // 设置Apollo Server
 const server=new ApolloServer({
     typeDefs,resolvers,
-    context:()=>{
-        // 把数据库模型添加到上下文中
-        return {models};
+    context:({req})=>{
+        // 从首部获取令牌
+        const token=req.headers.authorization;
+        // 尝试使用令牌检索用户
+        const user=getUser(token);
+        // 暂且把用户输出到控制台中
+        console.log('user',user);
+        // 把数据库模型和用户添加到上下文中
+        return {models,user};
     }
 })
 
