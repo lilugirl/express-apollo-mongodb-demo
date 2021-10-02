2cd34eed63677f5e791197421b361bcd67c94a2a
liu yi <yolaucn@gmail>
2021-10-02 12:48:22 +0800

    用户查询

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -6,5 +6,17 @@ module.exports={
     },
     note:async(parent,args,{models})=>{
        return await models.Noted.findById(args.id)
+    },
+    user:async(parent,{username},{models})=>{
+        // 查找指定用户对应的用户
+        return await models.User.findOne({username});
+    },
+    users:async(parent,args,{models})=>{
+        // 查找所有用户
+        return await models.User.find({});
+    },
+    me:async(parent,args,{models,user})=>{
+        // 查找上下文中的当前用户
+        return await models.User.findById(user.id)
     }
 }
